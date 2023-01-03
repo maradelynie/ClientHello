f46111a8ca9ec147e139aed317dd1b74bcf53037
Mara Oliveira <maradelynie@gmail.com>
2023-01-03 10:49:49 -0300

    fix eslint validation
    Fix a couple of validations on changed files so it runs without warnings

--- src/Views/Load/Index.tsx
+++ src/Views/Load/Index.tsx
@@ -1,71 +1,95 @@
-import "./style.scss";
-import { useEffect, useState } from "react";
+import './style.scss'
+import { useEffect, useState } from 'react'
 import * as api from '../../sevices'
-import ModalCadastroTorneio from "../../components/ModalCadastroTorneio";
-import { Edit } from 'react-feather';
-import { useNavigate } from "react-router-dom";
-import Backdrop from "../../components/backdrop";
-import Alert from "../../components/alert";
+import ModalCadastroTorneio from '../../components/ModalCadastroTorneio'
+import { Edit } from 'react-feather'
+import { useNavigate } from 'react-router-dom'
+import Backdrop from '../../components/backdrop'
+import Alert from '../../components/alert'
 
 type TournamentType = {
-  id: number, title: string, createdAt: string
+  id: number
+  title: string
+  createdAt: string
 }
 
 function Load() {
-  const navigate = useNavigate();
-  const [backdropStatus, setBackdropStatus] = useState(true);
-  const [AlertMessage, setMessageStatus] = useState('');
-  const [modalCadastroTorneio, setModalCadastroTorneio] = useState(false);
-  const [search, setSearch] = useState('');
-  const [tournaments, setTournaments] = useState<TournamentType[]>([]);
-  const [tournamentsToShow, setTournamentsToShow] = useState<TournamentType[]>([]);
+  const navigate = useNavigate()
+  const [backdropStatus, setBackdropStatus] = useState(true)
+  const [AlertMessage, setMessageStatus] = useState('')
+  const [modalCadastroTorneio, setModalCadastroTorneio] = useState(false)
+  const [search, setSearch] = useState('')
+  const [tournaments, setTournaments] = useState<TournamentType[]>([])
+  const [tournamentsToShow, setTournamentsToShow] = useState<TournamentType[]>(
+    []
+  )
 
   const getTournaments = async () => {
-    try{
+    try {
       setBackdropStatus(true)
       const tournaments = await api.getTorunaments()
       setTournaments(tournaments)
       setTournamentsToShow(tournaments)
       setSearch('')
-    }catch{
-      setMessageStatus("Erro ao carregar torneios")
-    }finally{
+    } catch {
+      setMessageStatus('Erro ao carregar torneios')
+    } finally {
       setBackdropStatus(false)
     }
-    
   }
-  const handleSearch = (term:string) => {
-    setTournamentsToShow( tournaments.filter(tournament=>tournament.title.toLocaleLowerCase().includes(term.toLocaleLowerCase()))
+  const handleSearch = (term: string) => {
+    setTournamentsToShow(
+      tournaments.filter(tournament =>
+        tournament.title.toLocaleLowerCase().includes(term.toLocaleLowerCase())
+      )
     )
     setSearch(term)
   }
 
   useEffect(() => {
     getTournaments()
-  }, []);
+  }, [])
 
   return (
     <div className="home_container">
-      <Backdrop open={backdropStatus}/>
-      <Alert open={!!AlertMessage} close={()=>setMessageStatus('')} message={AlertMessage}/>
+      <Backdrop open={backdropStatus} />
+      <Alert
+        open={!!AlertMessage}
+        close={() => setMessageStatus('')}
+        message={AlertMessage}
+      />
       <header>
-        <input type="search" value={search} onChange={e=>handleSearch(e.target.value)} placeholder='Pesquisar' />
+        <input
+          type="search"
+          value={search}
+          onChange={e => handleSearch(e.target.value)}
+          placeholder="Pesquisar"
+        />
       </header>
-          <section>
-            {tournamentsToShow.map(tournament=>{
-              return <div key={tournament.id} className="button load-tournament" onClick={()=>navigate('/tournament/'+tournament.id)}><h3>{tournament.title}</h3><span>{new Date(tournament.createdAt).toLocaleDateString()}</span></div>
-            })}
-          </section>
-          {modalCadastroTorneio ?
-          <ModalCadastroTorneio
-          setBackdropStatus={setBackdropStatus}
+      <section>
+        {tournamentsToShow.map(tournament => {
+          return (
+            <div
+              key={tournament.id}
+              className="button load-tournament"
+              onClick={() => navigate('/tournament/' + tournament.id)}
+            >
+              <h3>{tournament.title}</h3>
+              <span>{new Date(tournament.createdAt).toLocaleDateString()}</span>
+            </div>
+          )
+        })}
+      </section>
+      {modalCadastroTorneio ? (
+        <ModalCadastroTorneio
           open={modalCadastroTorneio}
           close={() => setModalCadastroTorneio(false)}
-          setMessageStatus={setMessageStatus}
-        />:''}
-        
+        />
+      ) : (
+        ''
+      )}
     </div>
-  );
+  )
 }
 
-export default Load;
+export default Load
--- src/Views/Match/Index.tsx
+++ src/Views/Match/Index.tsx
@@ -1,201 +1,201 @@
-import "./style.scss";
+import './style.scss'
 
-import * as api from "../../sevices";
-import Contador from "../../components/Contador";
-import { useEffect, useState } from "react";
-import ModalStart from "../../components/ModalStart/index";
-import ModalCount from "../../components/ModalCount/index";
-import { io } from "socket.io-client";
-import { useMatch } from "../../hooks/useMatch";
-import { useLocation, useNavigate } from "react-router-dom";
-import Backdrop from "../../components/backdrop";
-import Alert from "../../components/alert";
-import { RacerType } from "../../types/useMatch";
+import * as api from '../../sevices'
+import Contador from '../../components/Contador'
+import { useEffect, useState } from 'react'
+import ModalStart from '../../components/ModalStart/index'
+import ModalCount from '../../components/ModalCount/index'
+import { io } from 'socket.io-client'
+import { useMatch } from '../../hooks/useMatch'
+import { useLocation, useNavigate } from 'react-router-dom'
+import Backdrop from '../../components/backdrop'
+import Alert from '../../components/alert'
+import { RacerType } from '../../types/useMatch'
 
 const APIURLSOCKET =
-  process.env.REACT_APP_APIURLSOCKET || "http://localhost:3008";
+  process.env.REACT_APP_APIURLSOCKET || 'http://localhost:3008'
 
 function Home() {
-  const [modalStart, setModalStart] = useState(false);
-  const [backdropStatus, setBackdropStatus] = useState(false);
-  const [messageStatus, setMessageStatus] = useState("");
-  const { match, setupPlayers, players } = useMatch();
-  const { pathname } = useLocation();
-  const navigate = useNavigate();
-  const [playerA, setPlayerA] = useState<RacerType | false>(false);
-  const [p1Pulse, setP1Pulse] = useState(0);
-  const [playerB, setPlayerB] = useState<RacerType | false>(false);
-  const [p2Pulse, setP2Pulse] = useState(0);
-  const [countDown, setCountDown] = useState(3);
-  const [showCountDown, setShowCountDown] = useState(false);
+  const [modalStart, setModalStart] = useState(false)
+  const [backdropStatus, setBackdropStatus] = useState(false)
+  const [messageStatus, setMessageStatus] = useState('')
+  const { match, setupPlayers, players } = useMatch()
+  const { pathname } = useLocation()
+  const navigate = useNavigate()
+  const [playerA, setPlayerA] = useState<RacerType | false>(false)
+  const [p1Pulse, setP1Pulse] = useState(0)
+  const [playerB, setPlayerB] = useState<RacerType | false>(false)
+  const [p2Pulse, setP2Pulse] = useState(0)
+  const [countDown, setCountDown] = useState(3)
+  const [showCountDown, setShowCountDown] = useState(false)
 
-  const [startTimer, setStartTimer] = useState(false);
-  const [actualTime, setActualTime] = useState<number>(0);
+  const [startTimer, setStartTimer] = useState(false)
+  const [actualTime, setActualTime] = useState<number>(0)
 
-  const [quickPlayMode, setQuickPlayMode] = useState(false);
+  const [quickPlayMode, setQuickPlayMode] = useState(false)
 
   const handleRestart = () => {
-    setP2Pulse(0);
-    setP1Pulse(0);
-    setActualTime(0);
-    setStartTimer(false);
-    setModalStart(true);
-  };
+    setP2Pulse(0)
+    setP1Pulse(0)
+    setActualTime(0)
+    setStartTimer(false)
+    setModalStart(true)
+  }
   const handleCancelStart = () => {
-    navigate(-1);
-  };
+    navigate(-1)
+  }
   const handleInvert = () => {
     if (playerB && playerA) {
-      const newPlayerA = { ...playerB };
-      const newPlayerB = { ...playerA };
+      const newPlayerA = { ...playerB }
+      const newPlayerB = { ...playerA }
 
-      setPlayerA(newPlayerA);
-      setPlayerB(newPlayerB);
+      setPlayerA(newPlayerA)
+      setPlayerB(newPlayerB)
     }
-  };
+  }
   const startCountDown = async () =>
     await new Promise<void>((resolve, reject) => {
       const count = setInterval(() => {
-        setCountDown((actual) => {
-          const newValue = actual - 1;
+        setCountDown(actual => {
+          const newValue = actual - 1
           if (newValue === 0) {
-            clearInterval(count);
-            resolve();
-            setShowCountDown(false);
+            clearInterval(count)
+            resolve()
+            setShowCountDown(false)
 
-            return 3;
+            return 3
           }
-          return newValue;
-        });
-      }, 1000);
-    });
+          return newValue
+        })
+      }, 1000)
+    })
   const handleStart = async () => {
-    setModalStart(false);
-    setShowCountDown(true);
+    setModalStart(false)
+    setShowCountDown(true)
 
-    await startCountDown();
-    startGame();
-  };
+    await startCountDown()
+    startGame()
+  }
   const startGame = () => {
-    setStartTimer(true);
-  };
+    setStartTimer(true)
+  }
   const showTimer = (time: number) => {
     return (
       <>
         <span className="digits">
-          {("0" + Math.floor((time / 60000) % 60)).slice(-2)}:
+          {('0' + Math.floor((time / 60000) % 60)).slice(-2)}:
         </span>
         <span className="digits">
-          {("0" + Math.floor((time / 1000) % 60)).slice(-2)}.
+          {('0' + Math.floor((time / 1000) % 60)).slice(-2)}.
         </span>
         <span className="digits mili-sec">
-          {("0" + ((time / 10) % 100)).slice(-2)}
+          {('0' + ((time / 10) % 100)).slice(-2)}
         </span>
       </>
-    );
-  };
+    )
+  }
   const handleFinishMatch = async (
     playerA: RacerType,
     playerB: RacerType,
     winner: string
   ) => {
     try {
-      setBackdropStatus(true);
+      setBackdropStatus(true)
       if (match) {
         const raceData = {
           key: match.id,
           winner,
           runnerA: playerA.key,
           runnerB: playerB.key,
-          category: match.category,
-        };
-        await api.putPlayers([playerA, playerB], match.tournament);
-        await api.putRace(raceData, match.tournament);
+          category: match.category
+        }
+        await api.putPlayers([playerA, playerB], match.tournament)
+        await api.putRace(raceData, match.tournament)
 
-        const newPlayers = players.map((player) => {
-          if (player.key === playerA.key) return playerA;
-          else if (player.key === playerB.key) return playerB;
-          return player;
-        });
+        const newPlayers = players.map(player => {
+          if (player.key === playerA.key) return playerA
+          else if (player.key === playerB.key) return playerB
+          return player
+        })
 
-        setupPlayers(newPlayers);
-        navigate(-1);
+        setupPlayers(newPlayers)
+        navigate(-1)
       }
     } catch {
-      setMessageStatus("Erro ao carregar chaves");
+      setMessageStatus('Erro ao carregar chaves')
     } finally {
-      setBackdropStatus(false);
+      setBackdropStatus(false)
     }
-  };
+  }
 
   useEffect(() => {
-    let interval: ReturnType<typeof setInterval>;
+    let interval: ReturnType<typeof setInterval> = setInterval(() => null)
     interval = setInterval(() => {
       if (startTimer) {
-        setActualTime((time) => {
-          return time + 10;
-        });
+        setActualTime(time => {
+          return time + 10
+        })
       } else {
-        clearInterval(interval);
+        clearInterval(interval)
       }
-    }, 10);
+    }, 10)
 
     return () => {
-      clearInterval(interval);
-    };
-  }, [startTimer]);
+      clearInterval(interval)
+    }
+  }, [startTimer])
 
   useEffect(() => {
     if (startTimer) {
-      const socket = io(APIURLSOCKET, { transports: ["websocket", "polling"] });
+      const socket = io(APIURLSOCKET, { transports: ['websocket', 'polling'] })
 
-      socket.on("data", (data) => {
+      socket.on('data', data => {
         if (startTimer) {
-          if (data.includes("P2")) {
-            setP1Pulse((pulse) => pulse + 1);
-          } else if (data.includes("P1")) {
-            setP2Pulse((pulse) => pulse + 1);
+          if (data.includes('P2')) {
+            setP1Pulse(pulse => pulse + 1)
+          } else if (data.includes('P1')) {
+            setP2Pulse(pulse => pulse + 1)
           }
         }
-      });
+      })
 
       return () => {
-        socket.off("data", (data) => {
-          console.log(data);
-        });
-        socket.close();
-      };
+        socket.off('data', data => {
+          console.log(data)
+        })
+        socket.close()
+      }
     }
-  }, [startTimer]);
+  }, [startTimer])
 
   useEffect(() => {
-    if (pathname.toLocaleLowerCase().includes("quickplay")) {
+    if (pathname.toLocaleLowerCase().includes('quickplay')) {
       const defaultPlayer = {
         id: null,
         average_speed: 0,
         wos: 0,
         times_played: 0,
         victories: 0,
-        category: "sem",
-        tournament: "0",
-        dead: false,
-      };
-      setQuickPlayMode(true);
-      setPlayerA({ ...defaultPlayer, name: "Player 1" });
-      setPlayerB({ ...defaultPlayer, name: "Player 2" });
+        category: 'sem',
+        tournament: '0',
+        dead: false
+      }
+      setQuickPlayMode(true)
+      setPlayerA({ ...defaultPlayer, name: 'Player 1' })
+      setPlayerB({ ...defaultPlayer, name: 'Player 2' })
     } else if (match) {
-      setPlayerA(match.runnerA);
-      setPlayerB(match.runnerB);
-      setModalStart(true);
-    } else navigate("/");
+      setPlayerA(match.runnerA)
+      setPlayerB(match.runnerB)
+      setModalStart(true)
+    } else navigate('/')
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [])
 
   return (
     <div className="match-container">
       <Alert
         open={!!messageStatus}
-        close={() => setMessageStatus("")}
+        close={() => setMessageStatus('')}
         message={messageStatus}
       />
       <Backdrop open={backdropStatus} />
@@ -234,7 +234,7 @@ function Home() {
         invert={handleInvert}
       />
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
